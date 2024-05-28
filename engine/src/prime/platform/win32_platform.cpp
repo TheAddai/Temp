314860ffdff4edfabd5c5fe81af06431d4a9906f
TheAddai <agbonicholas1965@gmail.com>
2024-05-28 13:20:19 +0000

    callbacks

--- engine/src/prime/platform/win32_platform.cpp
+++ engine/src/prime/platform/win32_platform.cpp
@@ -15,6 +15,76 @@ namespace prime {
 		P_ERROR("GLFW Error ({0}): {1}", error, description);
 	}
 
+	static void SetCallbacks(GLFWwindow* windowHandle)
+	{
+		glfwSetWindowSizeCallback(windowHandle, [](GLFWwindow* window, int width, int height)
+			{
+				Platform::Data& data = *(Platform::Data*)glfwGetWindowUserPointer(window);
+				data.width = width;
+				data.height = height;
+
+				P_INFO("resize callback");
+			});
+
+		glfwSetWindowCloseCallback(windowHandle, [](GLFWwindow* window)
+			{
+				P_INFO("close callback");
+			});
+
+		glfwSetKeyCallback(windowHandle, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+			{
+				Platform::Data& data = *(Platform::Data*)glfwGetWindowUserPointer(window);
+
+				switch (action)
+				{
+				case GLFW_PRESS:
+				{
+					P_INFO("key pressed callback");
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					P_INFO("key released callback");
+					break;
+				}
+				case GLFW_REPEAT:
+				{
+					P_INFO("key repeat callback");
+					break;
+				}
+				}
+			});
+
+		glfwSetMouseButtonCallback(windowHandle, [](GLFWwindow* window, int button, int action, int mods)
+			{
+				Platform::Data& data = *(Platform::Data*)glfwGetWindowUserPointer(window);
+
+				switch (action)
+				{
+				case GLFW_PRESS:
+				{
+					P_INFO("mouse pressed callback");
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					P_INFO("mouse released callback");
+					break;
+				}
+				}
+			});
+
+		glfwSetScrollCallback(windowHandle, [](GLFWwindow* window, double xOffset, double yOffset)
+			{
+				P_INFO("scroll callback");
+			});
+
+		glfwSetCursorPosCallback(windowHandle, [](GLFWwindow* window, double xPos, double yPos)
+			{
+				P_INFO("cursor callback");
+			});
+	}
+
 	void Platform::Init(const std::string& title, ui32 width, ui32 height, b8 fullScreen)
 	{
 		i8 status = glfwInit();
@@ -29,6 +99,7 @@ namespace prime {
 		if (fullScreen) { glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE); }
 		m_windowHandle = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
 		glfwSetWindowUserPointer((GLFWwindow*)m_windowHandle, &m_data);
+		SetCallbacks((GLFWwindow*)m_windowHandle);
 	}
 
 	void Platform::Shutdown()
